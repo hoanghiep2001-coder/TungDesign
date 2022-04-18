d21c43b1d6bc1356bffd92c8dab859261c276923
This is Hiep <duong.hoanghiepp@gmail.com>
2022-04-19 06:13:33 +0700

    Hiep duong

---
+++ Content/js/common-pages/home.js
@@ -0,0 +1,158 @@
+$(function () {
+    let AsiaDevice = {
+        init: function () {
+            this.render()
+            this.onSomething()
+        },
+        onSomething: function() {
+            this.loadPage()
+            this.productDropdown()
+            this.loadingGif()
+            this.productItem()
+        },
+        render: function () {
+            this.renderProductItem()
+        },
+
+        // Array
+        productArr: [
+            {
+                image: '../Content/image/home/1.jpg',
+                name: 'Áp cao',
+            },
+            {
+                image: '../Content/image/home/2.jpg',
+                name: 'Áp thấp',
+            },
+            {
+                image: '../Content/image/home/1.jpg',
+                name: 'Áp cao',
+            },
+            {
+                image: '../Content/image/home/2.jpg',
+                name: 'Áp thấp',
+            },
+            {
+                image: '../Content/image/home/1.jpg',
+                name: 'Áp cao',
+            },
+            {
+                image: '../Content/image/home/2.jpg',
+                name: 'Áp thấp',
+            },
+            {
+                image: '../Content/image/home/1.jpg',
+                name: 'Áp cao',
+            },
+            {
+                image: '../Content/image/home/2.jpg',
+                name: 'Áp thấp',
+            },
+            {
+                image: '../Content/image/home/1.jpg',
+                name: 'Áp cao',
+            },
+            {
+                image: '../Content/image/home/2.jpg',
+                name: 'Áp thấp',
+            },
+            {
+                image: '../Content/image/home/1.jpg',
+                name: 'Áp cao',
+            },
+            {
+                image: '../Content/image/home/2.jpg',
+                name: 'Áp thấp',
+            },
+        ],
+
+        // Function
+        productDropdown() {
+            $('.dropdown-list').slideUp();
+
+            $('.link-item').click(function (e) { 
+                e.preventDefault();
+                let $this = $(this)
+                let $dropdown = $this.next()
+                
+                $($dropdown).slideToggle();
+                $('.dropdown-list').not($dropdown).slideUp();
+            });
+
+            $('.dropdown-list-item').click(function (e) { 
+                e.preventDefault();
+                let $this = $(this)
+
+                $($this).addClass('active');
+
+                $('.dropdown-list-item').not($this).removeClass('active')
+            });
+        },
+        loadingGif() {
+            $('.js-show-loading-gif').click(function (e) { 
+                e.preventDefault();
+                $('.js-loading-content').fadeOut()
+                $('.loading-bahavior').fadeIn()
+
+                setTimeout(function loadingOff() {
+                    $('.js-loading-content').fadeIn(1000)
+                    $('.loading-bahavior').fadeOut(200)
+                }, 2000)
+
+            });
+        },
+        productItem() {
+            $('.js-show-product').click(function (e) { 
+                e.preventDefault();
+
+                $('.product__container').removeClass('d-flex');
+                $('.product__container').hide();
+
+                setTimeout(function showProductItem() {
+                    $('.js-loading-content').hide();
+                    $('.product__container').addClass('d-flex');
+                    $('.product__container').show();
+                }, 2200)
+            });
+        },
+        loadPage() {
+            $('.js-back-to-home').click(function (e) { 
+                e.preventDefault();
+
+                $('.product__container').removeClass('d-flex');
+                $('.product__container').hide();
+                $('.js-loading-content').fadeOut()
+                $('.loading-bahavior').fadeIn()
+
+                setTimeout(function loadingOff() {
+                    $('.js-loading-content').fadeIn(1000)
+                    $('.loading-bahavior').fadeOut(200)
+                }, 2000)
+
+                setTimeout(function loadPage() {
+                    $('.js-loading-content').show();
+                    $('.product__container').hide();
+                }, 2200)
+            });
+        },
+        renderProductItem: function () {
+            let _this = this;
+            let productList = _this.productArr.map(function (item, index) {
+              return `
+                    <div class="col-3 mt24">
+                    <div class="card-item">
+                        <div class="overflow-auto overflow-hidden">
+                            <img src="${item.image}" alt="">
+                        </div>
+                        
+                        <p>${item.name}</p>
+                    </div>
+                </div>
+                `;
+            });
+            $(".product__container").html(productList.join(""));
+          },
+    }
+
+    AsiaDevice.init()
+});
